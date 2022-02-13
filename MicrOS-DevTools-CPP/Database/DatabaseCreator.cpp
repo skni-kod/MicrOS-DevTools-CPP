0ba5e2c24ddf4ddc34c95a29676ee7a805c1f539
AzuxDario <pawel_mibor95@tlen.pl>
2022-02-13 11:29:48 +0100

    Added second table with settings

--- MicrOS-DevTools-CPP/Database/DatabaseCreator.cpp
+++ MicrOS-DevTools-CPP/Database/DatabaseCreator.cpp
@@ -8,13 +8,45 @@ DatabaseCreator::DatabaseCreator(QObject *parent) : QObject(parent)
 void DatabaseCreator::createDatabase(QSqlDatabase &database)
 {
     QSqlQuery query(database);
-    query.exec("CREATE TABLE SystemVersion (id INTEGER NOT NULL PRIMARY KEY, component TEXT NOT NULL, version TEXT NOT NULL)");
+    // Table SystemVersion
+    query.exec("CREATE TABLE SystemVersion (id INTEGER NOT NULL PRIMARY KEY, component TEXT NOT NULL UNIQUE, version TEXT NOT NULL)");
     query.prepare("INSERT INTO SystemVersion (component, version) VALUES (:component, :version)");
     query.bindValue(":component", "Application version");
     query.bindValue(":version", APPLICATION_VERSION);
     query.exec();
     query.prepare("INSERT INTO SystemVersion (component, version) VALUES (:component, :version)");
     query.bindValue(":component", "Database version");
-    query.bindValue(":version", DATABASE_VERSION);
+    query.bindValue(":version", QString::number(DATABASE_VERSION));
+    query.exec();
+
+    // Table SystemSettings
+    query.exec("CREATE TABLE SystemSettings (id INTEGER NOT NULL PRIMARY KEY, setting TEXT NOT NULL UNIQUE, value TEXT, type TEXT NOT NULL)");
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "Repository location");
+    query.bindValue(":type", "S");
+    query.exec();
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "MSYS location");
+    query.bindValue(":type", "S");
+    query.exec();
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "QEMU location");
+    query.bindValue(":type", "S");
+    query.exec();
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "Floppy letter");
+    query.bindValue(":type", "S");
+    query.exec();
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "Compilation threads");
+    query.bindValue(":type", "I");
+    query.exec();
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "Debugger target");
+    query.bindValue(":type", "S");
+    query.exec();
+    query.prepare("INSERT INTO SystemSettings (setting, type) VALUES (:setting, :type)");
+    query.bindValue(":setting", "Windows version");
+    query.bindValue(":type", "S");
     query.exec();
 }
